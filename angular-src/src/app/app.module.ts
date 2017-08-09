a5f2d2353b76e7acae30787f4e5974e2829e5f67
xeratec <xeratec@localhost.localdomain>
2017-08-09 22:38:03 +0200

    + Front-End Register Functionality

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages'
 
 const appRoutes: Routes = [
@@ -35,10 +37,14 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService],
+  providers: [
+    ValidateService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ValidateService } from '../../services/validate.service';
 import { FlashMessagesService} from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +15,12 @@ export class RegisterComponent implements OnInit {
   email: String;
   password: String;
 
-  constructor(private validateService : ValidateService, private flashMessages: FlashMessagesService) { }
+  constructor(
+    private validateService : ValidateService,
+    private flashMessages: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -32,10 +39,21 @@ export class RegisterComponent implements OnInit {
       return false;
     }
 
+    // Validate Email
     if (!this.validateService.validateEmail(user.email)) {
       this.flashMessages.show("Please use a valid Email", {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
-  }
 
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessages.show("You are now registered and can log in", {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessages.show("Something went wrong", {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+  }
 }
